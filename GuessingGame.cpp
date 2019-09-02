f17ff989bf473412177e2d9d46dff8790d908f5d
Bobby Austin <baustin1@fandm.edu>
2019-09-02 14:38:59 -0400

    Guessing Game v1

---
+++ GuessingGame.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 42;
+    
+    for(;;)
+    {
+        int guess = 0;
+        cout << "Enter a guess: ";
+        cin >> guess;
+        if(guess == num)
+            break;
+        else if(guess < num)
+            cout << "Too low!" << endl;
+        else
+            cout << "Too high!" << endl;
+    }
+    cout << "Correct!" << endl;
+}
